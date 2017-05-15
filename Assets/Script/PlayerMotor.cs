8fe16735ff6a4321dc369f9fdcdaea1a326c608c
Javier Bullrich <jaierbullrich@gmail.com>
2017-05-15 00:30:34 -0300

    Finished first video

---
+++ Assets/Script/PlayerMotor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// by @Bullrich
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+
+	private Rigidbody rb;
+	private Vector3 velocity = Vector3.zero;
+
+	private void Start(){
+		rb = GetComponent<Rigidbody>();
+	}
+
+	///<summary>Get a movement vector</summary>
+	public void Move(Vector3 _velocity){
+		velocity = _velocity;
+	}
+
+	void FixedUpdate(){
+		PerformMovement(velocity);
+	}
+
+	private void PerformMovement(Vector3 _velocity){
+		if (_velocity != Vector3.zero){
+			rb.MovePosition(rb.position + _velocity * Time.fixedDeltaTime);
+		}
+	}
+}
