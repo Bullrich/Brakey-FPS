be66cf7d48ec37a2b9d8d35486f7b71d53d5a2db
Javier Bullrich <javierbullrich@gmail.com>
2017-06-08 00:57:03 -0300

    Finished E17

---
+++ Assets/Script/HostGame.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+// by @Bullrich
+
+namespace game
+{
+    public class HostGame : MonoBehaviour
+    {
+        [SerializeField] private uint roomSize = 6;
+
+        private string roomName;
+
+        private NetworkManager networkManager;
+
+        private void Start()
+        {
+            networkManager = NetworkManager.singleton;
+            if (networkManager.matchMaker == null)
+                networkManager.StartMatchMaker();
+        }
+
+        public void SetRoomName(string _name)
+        {
+            roomName = _name;
+        }
+
+        public void CreateRoom()
+        {
+            if (!string.IsNullOrEmpty(roomName))
+            {
+                Debug.Log(string.Format("Creating Room: {0} with room for {1} players.", roomName, roomSize));
+                // Create room
+                networkManager.matchMaker.CreateMatch(
+                    roomName, roomSize, true, "", "", "", 0, 0, networkManager.OnMatchCreate);
+            }
+        }
+    }
+}
