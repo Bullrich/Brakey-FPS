8df0f643118fcfc3942c0463a0079e03080542c4
Javier Bullrich <javierbullrich@gmail.com>
2017-06-10 19:05:02 -0300

    Finished E20 - Join game

---
+++ Assets/Script/RoomListItem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking.Match;
+using UnityEngine.UI;
+
+// by @Bullrich
+
+namespace game
+{
+	public class RoomListItem : MonoBehaviour
+	{
+		public delegate void JoinRoomDelegate(MatchInfoSnapshot _match);
+
+		private JoinRoomDelegate joinRoomCallback;
+		
+		[SerializeField] private Text roomNameText;
+		private MatchInfoSnapshot match;
+
+		public void Setup(MatchInfoSnapshot _match, JoinRoomDelegate _joinRoomCallback)
+		{
+			match = _match;
+
+			roomNameText.text = match.name + string.Format(" ({0}/{1})", match.currentSize, match.maxSize);
+
+			joinRoomCallback = _joinRoomCallback;
+		}
+
+		public void JoinRoom()
+		{
+			joinRoomCallback.Invoke(match);
+		}
+
+	}
+}
